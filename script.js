89e1967af21779bc15ce64939ff47e901d7d9ff6
rbhogal <rbhogal23gmail.com>
2020-09-21 10:42:32 -0700

    Primatives vs objects

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Function constructor
-
+/*
 var john = {
     name: 'John',
     yearOfBirth: 1990,
@@ -53,5 +53,67 @@ Team.prototype.headCoach = this.headCoach;
 console.log(lakers);
 
 lakers.headCoach = "Frank Vogel";
+*/
 
+
+/*
+// Object.create
+
+var personProto = {
+    calculateAge: function() {
+        console.log(2016 - this.yearOfBirth);
+    }
+};
+
+var john = Object.create(personProto);
+
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
+
+var jane = Object.create(personProto,
+    {
+        name: { value: 'Jane'},
+        yearOfBirth: { value: 1969 },
+        job: {value: 'designer'}
+    });
+
+*/
+
+// Priitives vs objects
+
+
+// Primatives
+var a = 23;
+var b = a;
+a = 46;
+console.log(a,b);
+
+
+// Objects
+var obj1 = {
+    name: 'John',
+    age: 26
+};
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age);
+console.log(obj2.age);
+
+// Functions
+var age = 27;
+var obj = {
+    name: 'Jonas',
+    city: 'Lisbon'
+};
+
+function change(a, b) {
+    a = 30;
+    b.city = 'San Francisco';
+}
+
+change(age, obj);
+
+console.log(age);
+console.log(obj.city);
 
