aca3a2ceaf8b97eba2011db883b99c493d78b492
rbhogal <rbhogal23gmail.com>
2020-09-21 12:56:21 -0700

    Callback funtions

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ var jane = Object.create(personProto,
 
 // Priitives vs objects
 
-
+/*
 // Primatives
 var a = 23;
 var b = a;
@@ -117,3 +117,138 @@ change(age, obj);
 console.log(age);
 console.log(obj.city);
 
+*/
+/////////////////////////////////////////////
+// Lecture: Passing functions as arguments
+/*
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (var i = 0; i < arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calculateAge(el) {
+    return 2016 - el;
+}
+
+function isFullAge(el) {
+    return el >= 18;
+}
+
+function maxHeartRate(el) {
+    if (el >=18 && el <= 81) {
+    return Math.round(206.9 - (0.67 * el));
+    } else {
+        return -1;
+    }
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullAges = arrayCalc(ages, isFullAge);
+var rates = arrayCalc(ages, maxHeartRate);
+
+console.log(ages);
+console.log(fullAges);
+console.log(rates);
+
+*/
+
+
+/////////////////////////////////////////////
+// Lecture: Functions returning functions
+/*
+function interviewQuestion(job) {
+    if (job === 'designer') {
+        return function(name) {    // Anonymous function
+            console.log(name + ', can you please explain UX design is?');
+        }
+    } else if (job === 'teacher') {
+        return function(name) {
+            console.log('What subject do you teach, ' + name + '?');
+        }
+    } else {
+        return function(name) {
+            console.log('Hello ' + name + ', what do you do?');
+        }
+    } 
+}
+
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
+
+teacherQuestion('John');
+designerQuestion('John');
+designerQuestion('Jane');
+designerQuestion('Mark');
+designerQuestion('Mike');
+
+interviewQuestion('teacher')('Mark');
+
+
+function playerQuestion(position) {
+    if (position === 'point guard') {
+        return function(name) {
+            console.log(name + ' plays point guard.');
+        }
+    } else {
+        return function(name) {
+            console.log('Plays something else');
+        }
+    }
+};
+
+var playerPointGuard = playerQuestion('point guard');
+
+playerPointGuard('Rondo');
+
+
+*/
+
+
+////////////////////////////////////////
+// Lecture: IIFE - immedieatly invoked funciotn expression 
+
+/*
+function game() {
+    var score = Math.random() *10;
+    console.log(score >= 5);
+}
+
+game();
+*/
+
+/*
+
+(function () {
+    var score = Math.random() *10;
+    console.log(score >= 5);
+})();
+
+// console.log(score);
+
+(function (goodLuck) {
+    var score = Math.random() *10;
+    console.log(score >= 5 - goodLuck);
+})(5);
+
+*/
+
+////////////////////////////////////
+// Lectures: Closures
+
+function retirement(retirementAge){
+    var a = ' years left until retirment.';
+    return function(yearOfBirth) {
+        var age = 2016 - yearOfBirth;
+        console.log((retirementAge - age) + a);
+    }
+}
+
+var retirementUSA = retirement(66);
+retirementUSA(1990);
+
+retirement(66)(1990);
